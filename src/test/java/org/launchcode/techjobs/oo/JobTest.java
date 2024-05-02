c0aad42bee9567a3fb273db706f985eb8f169813
Kyle McColgan <kdmbus21@gmail.com>
2024-05-02 17:56:49 -0500

    Add unit test for full constructor (task #4 part two).

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -2,8 +2,7 @@ package org.launchcode.techjobs.oo;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 public class JobTest
 {
@@ -11,10 +10,28 @@ public class JobTest
     @Test
     public void testSettingJobId()
     {
-        //assertFalse(false);
         Job fullTimeJob = new Job();
         Job secondJob = new Job();
 
         assertNotEquals(fullTimeJob.getId(), secondJob.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields()
+    {
+        Job fullTimeJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertEquals(fullTimeJob.getName(), "Product tester");
+        assertEquals(fullTimeJob.getEmployer().getValue(), "ACME");
+        assertEquals(fullTimeJob.getLocation().getValue(), "Desert");
+        assertEquals(fullTimeJob.getPositionType().getValue(), "Quality control");
+        assertEquals(fullTimeJob.getCoreCompetency().getValue(), "Persistence");
+
+        assertTrue(fullTimeJob instanceof Job);
+        assertTrue(fullTimeJob.getEmployer() instanceof Employer);
+        assertTrue(fullTimeJob.getLocation() instanceof Location);
+        assertTrue(fullTimeJob.getPositionType() instanceof PositionType);
+        assertTrue(fullTimeJob.getCoreCompetency() instanceof CoreCompetency);
+
     }
 }
