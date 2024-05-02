31a764bc4c9bc0510c37b1a788a31d5379f39970
Kyle McColgan <kdmbus21@gmail.com>
2024-05-02 17:42:57 -0500

    Add unit test for empty constructor (task #4).

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,20 @@
 package org.launchcode.techjobs.oo;
 
-public class JobTest {
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+
+public class JobTest
+{
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId()
+    {
+        //assertFalse(false);
+        Job fullTimeJob = new Job();
+        Job secondJob = new Job();
+
+        assertNotEquals(fullTimeJob.getId(), secondJob.getId());
+    }
 }
